41b61d4d2c28b73eaf23d0ef6526c94ee9cc5d72
PC-1 <danilnub01@gmail.com>
2022-09-29 19:53:14 +0700

    new version DZ2

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -3,27 +3,9 @@
 // Напишите программу, которая принимает на вход координаты двух 
 // точек и находит расстояние между ними в 3D пространстве.
 
-int x1 = Coordinat("x", "A");
-int y1 = Coordinat("y", "A");
-int z1 = Coordinat("z", "A");
-int x2 = Coordinat("x", "B");
-int y2 = Coordinat("y", "B");
-int z2 = Coordinat("z", "B");
-
-int Coordinat(string pervoe, string vtoroe)
-{
-    Console.Write($"Введите координату {pervoe} точки {vtoroe}: ");
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-double Otvet(double x1, double x2, 
-                double y1, double y2, 
-                double z1, double z2){
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-                   Math.Pow((y2-y1), 2) + 
-                   Math.Pow((z2-z1), 2));
-}
-
-double dlinaLength =  Math.Round (Otvet(x1, x2, y1, y2, z1, z2), 2 );
-
-Console.WriteLine($"Расстояние между точками:  {dlinaLength}");
+Console.WriteLine("Введите координаты первой точки:  ");
+double [] t1 = new double[3] {Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine())};
+Console.WriteLine("Введите координаты второй точки");
+double [] t2 = new double[3] {Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine())};
+double schet = Math.Pow((t2[0]-t1[0]),2) + Math.Pow((t2[1]-t1[1]), 2) + Math.Pow((t2[2]-t1[2]), 2);
+Console.WriteLine(Math.Sqrt(schet));
