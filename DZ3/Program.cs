33f95d9718bd2d7e2a50f88f79a9b40e70a1c9d7
PC-1 <danilnub01@gmail.com>
2022-09-29 19:53:50 +0700

    add DZ3

---
+++ DZ3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на 
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите N:  ");
+double n = Convert.ToInt32(Console.ReadLine());
+double i = 1;
+while (i<=n){
+    if (i != n) Console.Write($"{Math.Pow((i),3)}, ");
+    else Console.Write($"{Math.Pow((i),3)}, ");
+    i++;
+}
